e87c5910e1626683f7d848d76fc47accc305862c
Nara Lewinsky <naralewinskee@gmail.com>
2019-08-04 16:11:52 +0300

    ... second step.

---
+++ spec/testcafe-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestcafeRunnerView from '../lib/testcafe-runner-view';
+
+describe('TestcafeRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
